99bcab868e948c128a788f5675a391e28b1b6371
Varun Kumar <varun.kumar12aug@gmail.com>
2025-12-13 15:39:25 +0530

    Added first time

---
+++ src/main/java/com/learning/ai/SpringAiDmrApplication.java
@@ -0,0 +1,13 @@
+package com.learning.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiDmrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiDmrApplication.class, args);
+	}
+
+}
