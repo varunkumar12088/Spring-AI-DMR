1ed100fb985aa63a4b6a6f7797e89b7c76b6fcf3
Varun Kumar <varun.kumar12aug@gmail.com>
2025-12-16 11:54:26 +0530

    Added Action toold

--- src/main/java/com/learning/ai/tools/SimpleDateTimeTool.java
+++ src/main/java/com/learning/ai/tools/SimpleDateTimeTool.java
@@ -2,9 +2,11 @@ package com.learning.ai.tools;
 
 import com.learning.ai.util.LogUtil;
 import org.springframework.ai.tool.annotation.Tool;
+import org.springframework.ai.tool.annotation.ToolParam;
 import org.springframework.context.i18n.LocaleContextHolder;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class SimpleDateTimeTool {
 
@@ -15,5 +17,12 @@ public class SimpleDateTimeTool {
         return LocalDateTime.now()
                 .atZone(LocaleContextHolder.getTimeZone().toZoneId())
                 .toString();
+    }
+
+    //Action tool
+    @Tool(description = "Set the alarm for given time.")
+    public void setAlarm(@ToolParam(description = "Time in ISO-8601 format") String time){
+        var dateTime = LocalDateTime.parse(time, DateTimeFormatter.ISO_DATE_TIME);
+        LogUtil.log("calling setAlarm :"  + dateTime);
     }
 }
